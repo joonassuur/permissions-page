9852bdcf9315e1e93652be03b65050f1beeec3a9
JoonasSuurvali <joonas.suurvali@concise.ee>
2024-04-08 10:26:00 +0300

    env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
+const dev = process.env.NODE_ENV === 'development';
 export default defineConfig({
   plugins: [react()],
+  base: dev ? '' : '/permissions-page/',
 });
